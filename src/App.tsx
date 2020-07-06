74ae96df9edd4554df0be0942c92351a71953e14
Raphael Alvarenga <raphaelalvarenga2@gmail.com>
2020-07-06 12:01:18 -0300

    [Icons & Toolbar] THe Ant Design Icons has been installed, a toolbar has been created to be used later and the CSS file has been moved to a folder called assets.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ListProducts from "./views/ListProducts";
-import './App.css';
+import './assets/styles.css';
 
 const App = () => <ListProducts />
 
--- src/views/ListProducts.tsx
+++ src/views/ListProducts.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import ResponseInterface from "../interfaces/response-interface";
 import { ProductInterface } from "../interfaces/product-interface";
+import { PageHeader, Button, Drawer } from "antd";
+import { PoweroffOutlined } from "@ant-design/icons";
 
 const ListProducts = () => {
 
@@ -19,7 +21,7 @@ const ListProducts = () => {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
-                "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjoiYTlmYjQxMzBkZmFiMTcwMjllMjE1NThlMmY3ODZiZTgiLCJpYXQiOjE1OTM5OTA1NjMsImV4cCI6MTU5Mzk5NDE2M30.pix_lBDzUIMj4TvwgZf7rlYltrMpu4EIikAHgxI_m6A",
+                "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjoiMjY3ZmU5NGNmNWU0YTk0YmRmZGE3ZmNhZDUzZGNiNTMiLCJpYXQiOjE1OTM5OTM3NzksImV4cCI6MTU5Mzk5NzM3OX0.QFBK3W4vQXyjB3V70ZfXk8Dfd7FQj0bjc2WKJjFvpNw",
                 "action": "listProducts",
                 "idLogin": 1,
                 "params": {
@@ -38,7 +40,16 @@ const ListProducts = () => {
         setProducts(response.params);
     }
     
-    return <h1>List of Products</h1>
+    return (
+        <PageHeader
+            className = "site-page-header"
+            title = "Teste"
+            subTitle="This is a subtitle"
+            extra = {[
+                <Button key = "1" type = "text" shape = "circle" danger icon = {<PoweroffOutlined />} />
+            ]}
+        />
+    )
 }
 
 export default ListProducts;
