20e0555ad1cfaf18ed16b75482d037903b5314a2
Raphael Alvarenga <raphaelalvarenga2@gmail.com>
2020-07-07 00:54:17 -0300

    [Hard Code & Table] A Hard Code has been created to avoid external requests and a custom table has been created.

--- src/views/ListProducts.tsx
+++ src/views/ListProducts.tsx
@@ -3,32 +3,259 @@ import ResponseInterface from "../interfaces/response-interface";
 import { ProductInterface } from "../interfaces/product-interface";
 import config from "../routines/config";
 import auth from "../routines/auth";
-import { PageHeader, Button } from "antd";
+import { PageHeader, Button, Space } from "antd";
 import { MenuOutlined, PoweroffOutlined } from "@ant-design/icons";
 import { RouteComponentProps } from "react-router-dom";
-import { Table, Space } from "antd";
+import { Row, Col } from "antd";
 
 const ListProducts: FunctionComponent<RouteComponentProps> = (props) => {
 
-    const [products, setProducts] = useState<ProductInterface[]>([]);
+    const [products, setProducts] = useState<ProductInterface[]>([
+        {
+            "idProduct": 1,
+            "name": "Frontier",
+            "description": "2008",
+            "category": "Nissan",
+            "price": "$288486.27",
+            "status": 1
+        },
+        {
+            "idProduct": 2,
+            "name": "Ranger",
+            "description": "1995",
+            "category": "Ford",
+            "price": "$463152.42",
+            "status": 1
+        },
+        {
+            "idProduct": 3,
+            "name": "Grand Prix",
+            "description": "1999",
+            "category": "Pontiac",
+            "price": "$49478.41",
+            "status": 1
+        },
+        {
+            "idProduct": 4,
+            "name": "Aerostar",
+            "description": "1992",
+            "category": "Ford",
+            "price": "$256064.23",
+            "status": 1
+        },
+        {
+            "idProduct": 5,
+            "name": "Eurovan",
+            "description": "2001",
+            "category": "Volkswagen",
+            "price": "$136835.56",
+            "status": 1
+        },
+        {
+            "idProduct": 6,
+            "name": "Lanos",
+            "description": "1999",
+            "category": "Daewoo",
+            "price": "$637850.13",
+            "status": 1
+        },
+        {
+            "idProduct": 7,
+            "name": "Outback",
+            "description": "2009",
+            "category": "Subaru",
+            "price": "$701557.02",
+            "status": 1
+        },
+        {
+            "idProduct": 8,
+            "name": "Aurora",
+            "description": "2001",
+            "category": "Oldsmobile",
+            "price": "$29620.86",
+            "status": 1
+        },
+        {
+            "idProduct": 9,
+            "name": "Continental",
+            "description": "2010",
+            "category": "Bentley",
+            "price": "$695516.77",
+            "status": 1
+        },
+        {
+            "idProduct": 10,
+            "name": "Reatta",
+            "description": "1990",
+            "category": "Buick",
+            "price": "$832108.56",
+            "status": 1
+        },
+        {
+            "idProduct": 11,
+            "name": "Tundra",
+            "description": "2009",
+            "category": "Mitsubishi",
+            "price": "$984629.10",
+            "status": 1
+        },
+        {
+            "idProduct": 12,
+            "name": "Suburban 2500",
+            "description": "1996",
+            "category": "Chevrolet",
+            "price": "$357960.94",
+            "status": 1
+        },
+        {
+            "idProduct": 13,
+            "name": "Mazda3",
+            "description": "2011",
+            "category": "Mazda",
+            "price": "$124051.30",
+            "status": 1
+        },
+        {
+            "idProduct": 14,
+            "name": "Monte Carlo",
+            "description": "2004",
+            "category": "Chevrolet",
+            "price": "$253250.38",
+            "status": 1
+        },
+        {
+            "idProduct": 15,
+            "name": "929",
+            "description": "1995",
+            "category": "Mazda",
+            "price": "$946244.70",
+            "status": 1
+        },
+        {
+            "idProduct": 16,
+            "name": "Avalon",
+            "description": "2007",
+            "category": "Toyota",
+            "price": "$208023.10",
+            "status": 1
+        },
+        {
+            "idProduct": 17,
+            "name": "A4",
+            "description": "1997",
+            "category": "Audi",
+            "price": "$553787.96",
+            "status": 1
+        },
+        {
+            "idProduct": 18,
+            "name": "Grand Marquis",
+            "description": "1993",
+            "category": "Mercury",
+            "price": "$354244.52",
+            "status": 1
+        },
+        {
+            "idProduct": 19,
+            "name": "1500",
+            "description": "1998",
+            "category": "Chevrolet",
+            "price": "$59619.59",
+            "status": 1
+        },
+        {
+            "idProduct": 20,
+            "name": "Ram 1500",
+            "description": "2009",
+            "category": "Dodge",
+            "price": "$262897.58",
+            "status": 1
+        },
+        {
+            "idProduct": 21,
+            "name": "RDX",
+            "description": "2010",
+            "category": "Acura",
+            "price": "$586308.21",
+            "status": 1
+        },
+        {
+            "idProduct": 22,
+            "name": "Outback",
+            "description": "2002",
+            "category": "Subaru",
+            "price": "$207736.33",
+            "status": 1
+        },
+        {
+            "idProduct": 23,
+            "name": "900",
+            "description": "1995",
+            "category": "Saab",
+            "price": "$481460.50",
+            "status": 1
+        },
+        {
+            "idProduct": 24,
+            "name": "F150",
+            "description": "1993",
+            "category": "Ford",
+            "price": "$772956.39",
+            "status": 1
+        },
+        {
+            "idProduct": 25,
+            "name": "F-Series",
+            "description": "1990",
+            "category": "Ford",
+            "price": "$940121.07",
+            "status": 1
+        },
+        {
+            "idProduct": 26,
+            "name": "Pathfinder",
+            "description": "1997",
+            "category": "Nissan",
+            "price": "$920561.21",
+            "status": 1
+        },
+        {
+            "idProduct": 27,
+            "name": "626",
+            "description": "2001",
+            "category": "Mazda",
+            "price": "$232629.12",
+            "status": 1
+        },
+        {
+            "idProduct": 28,
+            "name": "Silverado 2500",
+            "description": "2010",
+            "category": "Chevrolet",
+            "price": "$401676.33",
+            "status": 1
+        },
+        {
+            "idProduct": 29,
+            "name": "Mystique",
+            "description": "1998",
+            "category": "Mercury",
+            "price": "$699181.58",
+            "status": 1
+        },
+        {
+            "idProduct": 30,
+            "name": "Grand Cherokee",
+            "description": "2011",
+            "category": "Jeep",
+            "price": "$772844.18",
+            "status": 1
+        }
+    ]);
 
     useEffect(() => {
-        getProducts()
+        // getProducts()
     }, []);
-
-    const columns = [
-        {title: "ID", dataIndex: "idProduct", key: "id"},
-        {title: "Name", dataIndex: "name", key: "name"},
-        {title: "Description", dataIndex: "description", key: "description"},
-        {title: "Category", dataIndex: "category", key: "category"},
-        {title: "Price", dataIndex: "price", key: "price"},
-        {title: "Actions", key: "action", render: () => (
-            <Space>
-                <Button >Edit</Button>
-                <Button danger>Delete</Button>
-            </Space>
-        )}
-    ]
 
     const getProducts = async () => {
         const endpoint: string = config.url;
@@ -81,7 +308,45 @@ const ListProducts: FunctionComponent<RouteComponentProps> = (props) => {
                     />
                 ]}
             />}
-            <Table columns = {columns} dataSource = {products} />
+
+            <div style = {{maxWidth: "1200px", margin: "auto"}}>
+                <Row>
+                    <Col span = {4}></Col>
+                    <Col span = {4}>Name</Col>
+                    <Col span = {4}>Description</Col>
+                    <Col span = {4}>Category</Col>
+                    <Col span = {4}>Price</Col>
+                    <Col span = {4} style = {{textAlign: "center"}}>Actions</Col>
+                </Row>
+                {
+                    products.map(product => (
+                        <React.Fragment key = {product.idProduct}>
+                            {product.status === 1 &&
+                            <Row
+                                
+                                align = "middle"
+                                style = {{
+                                    backgroundColor: product.idProduct % 2 === 0 ? "rgb(240, 240, 240)" : "white"
+                                }}>
+                                <Col span = {4}>
+                                    <img src = {require("../images/caravatar120x90.png")} />
+                                </Col>
+                                <Col span = {4}>{product.name}</Col>
+                                <Col span = {4}>{product.description}</Col>
+                                <Col span = {4}>{product.category}</Col>
+                                <Col span = {4}>{product.price}</Col>
+                                <Col span = {4} style = {{textAlign: "center"}}>
+                                    <Space>
+                                        <Button>Edit</Button>
+                                        <Button danger>Delete</Button>
+                                    </Space>
+                                </Col>
+                            </Row>
+                            }
+                        </React.Fragment>
+                    ))
+                }
+            </div>
         </>
     )
 }
